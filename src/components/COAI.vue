b13e84aee5364a873281dae0024af9e9a7661fe2
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-09-06 18:33:39 +0200

    fetch data from remote url

--- src/components/COAI.vue
+++ src/components/COAI.vue
@@ -1,45 +1,63 @@
 <script setup>
 import COAITile from './COAITile.vue'
-import demoList from '../coai.json'
+import offlineList from '../../public/coai.json'
+import { ref } from 'vue'
+import axios from 'axios'
+
+let coaiList = ref([])
+let jsonUrl = 'https://iris.informatik.uni-bremen.de/data/coai.json'
+
+if (import.meta.env.DEV) {
+  jsonUrl = 'coai.json'
+}
 
 function randomDate() {
   const start = 1683995706000
   const end = 1693905934063
-  
+
   return Math.round(Math.random() * (end - start)) + start
 }
 
-const validatedList = demoList.map (e => {
-  let _obj = {
-    title: '',
-    cover_img: '',
-    description: '',
-    link: '',
-    src_url: '',
-    open_ease: '',
-    run_url: '',
-    asset_url: '',
-    author: 'unknown',
-    created_time: randomDate(),
-    environment: [],
-    ...e
-  }
-  _obj.environment = _obj.environment.map(env => {
-    return {
-      name: '',
-      robot: [],
-      task: [],
-      ...env
+function dataValidate(data) {
+  return data.map(e => {
+    let _obj = {
+      title: '',
+      cover_img: '',
+      description: '',
+      link: '',
+      src_url: '',
+      open_ease: '',
+      run_url: '',
+      asset_url: '',
+      author: 'unknown',
+      created_time: randomDate(),
+      environment: [],
+      ...e
     }
+    _obj.environment = _obj.environment.map(env => {
+      return {
+        name: '',
+        robot: [],
+        task: [],
+        ...env
+      }
+    })
+    return _obj
   })
-  return _obj
+}
+
+axios.get(jsonUrl).then(response => {
+  coaiList.value = dataValidate(response.data)
+}).catch(err => {
+  console.log(err)
+  coaiList.value = dataValidate(offlineList)
 })
 
 </script>
 
 <template>
   <div class="coai-list">
-    <COAITile v-for="(item, index) in validatedList" :item="item" :index="index"/>
+    <COAITile v-for="(item, index) in coaiList" :item="item" :index="index" />
   </div>
 </template>
 
