fd042dea7b8356c3ed19609cf02cf19a972bf8c6
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-09-20 18:40:14 +0200

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/moodle-sub-pages/',
+  base: '/coai-list/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
