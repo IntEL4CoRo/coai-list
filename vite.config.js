dd7f5d6f1d01bb4cf7f2e52c961d14bb715a3f41
yxzhan <yanxiang@uni-bremen.de>
2024-03-11 12:57:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/coai-list/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
